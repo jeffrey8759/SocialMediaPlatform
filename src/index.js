c6209fa8b56d7247261bb43bfc3bf80c04f860e8
Jeffrey Cheng <chengj23@uw.edu>
2020-07-28 16:19:33 -0700

    login prototype

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-mport React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class Main extends React.Component {
+  render() {
+    return (
+			<form action="action_page.php" method="post">
+ 				<div class="imgcontainer">
+  			</div>
+
+  			<div class="container">
+    			<label for="uname"><b>Username</b></label>
+    			<input type="text" placeholder="Enter Username" name="uname" required/>
+
+    			<label for="psw"><b>Password</b></label>
+    			<input type="password" placeholder="Enter Password" name="psw" required/>
+
+    			<button type="submit">Login</button>
+    			<label>
+      			<input type="checkbox" name="remember"/> Remember me
+    			</label>
+  			</div>
+
+  			<div class="container" style={{backgroundColor:'#f1f1f1'}}>
+    			<button type="button" class="cancelbtn">Cancel</button>
+    			<span class="psw">Forgot <a href="#">password?</a></span>
+  			</div>
+			</form>
+		)
+	}	
+}
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('root')
+);
+
